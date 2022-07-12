45a6bc7a8e8e6d49bef1a8c27ad6cc416bba757a
lastnamehurt <clhurt1@gmail.com>
2022-07-11 19:17:44 -0700

    rm imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,9 @@ import * as React from "react";
 import {
   ChakraProvider,
   extendTheme,
-  Box,
-  Text,
-  Link,
   VStack,
-  Code,
-  Grid,
   theme as baseTheme,
-  Flex,
 } from "@chakra-ui/react";
-import { ColorModeSwitcher } from "./ColorModeSwitcher";
-import { Logo } from "./Logo";
-import CardWithIllustration from "./components/QuoteForm";
-import VerticalLabels from "./components/Wizard";
 import { StepsStyleConfig as Steps } from "chakra-ui-steps";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
